9dd760367f71f313264f1e63b2b0042d11bb8583
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-07-26 17:57:53 +0100

    Add basics for heroku.

--- wallingford_castle/wsgi.py
+++ wallingford_castle/wsgi.py
@@ -1,14 +1,8 @@
-"""
-WSGI config for wallingford_castle project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wallingford_castle.settings")
 
+
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling, MediaCling
+
+application = Cling(MediaCling(get_wsgi_application()))
