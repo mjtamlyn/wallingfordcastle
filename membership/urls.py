e851de9e0efc8ccc49987423ec1f286f628cc8d4
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-09-27 19:54:25 +0100

    Add edit details form for Member.

--- membership/urls.py
+++ membership/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url('^$', views.Overview.as_view(), name='overview'),
+    url('^update/(?P<member_id>\d+)/$', views.MemberUpdate.as_view(), name='member-update'),
 ]
--- membership/views.py
+++ membership/views.py
@@ -1,6 +1,13 @@
-from django.views.generic import TemplateView
+import json
 
-from braces.views import LoginRequiredMixin
+from django.conf import settings
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.views.generic import TemplateView, UpdateView
+
+from braces.views import LoginRequiredMixin, MessageMixin
+import requests
+
+from .forms import MemberForm
 
 
 class Overview(LoginRequiredMixin, TemplateView):
@@ -10,3 +17,35 @@ class Overview(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['members'] = self.request.user.members.all()
         return context
+
+
+class MemberUpdate(LoginRequiredMixin, MessageMixin, UpdateView):
+    template_name = 'membership/member-update.html'
+    form_class = MemberForm
+    pk_url_kwarg = 'member_id'
+    success_url = reverse_lazy('membership:overview')
+
+    def get_queryset(self):
+        return self.request.user.members.all()
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self.messages.success('Details successfully updated!')
+        if settings.SLACK_MEMBERSHIP_HREF:
+            data = json.dumps({
+                'icon_emoji': ':up:',
+                'text': '%s has updated details!\n%s' % (
+                    form.cleaned_data['name'],
+                    self.request.build_absolute_uri(
+                        reverse(
+                            'admin:membership_member_change',
+                            args=(form.instance.pk,),
+                        )
+                    ),
+                )
+            })
+            try:
+                requests.post(settings.SLACK_MEMBERSHIP_HREF, data=data)
+            except Exception:
+                pass
+        return response
