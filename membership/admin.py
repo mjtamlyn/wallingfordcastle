63473ddeadb804693a644c55c5aecbec5e2e8928
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-10-03 17:34:04 +0100

    Improve admin display.

--- membership/admin.py
+++ membership/admin.py
@@ -5,6 +5,10 @@ from .models import Member
 
 @admin.register(Member)
 class MembershipInterestAdmin(admin.ModelAdmin):
-    list_display = ['name', 'age', 'membership_type', 'paid_until']
+    list_display = ['name', 'age', 'membership_type', 'has_payment_setup']
     list_filter = ['membership_type', 'age']
     readonly_fields = ['created', 'modified']
+
+    def has_payment_setup(self, instance):
+        return bool(instance.user.customer_id)
+    has_payment_setup.boolean = True
