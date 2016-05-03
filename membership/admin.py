e23d78fb8eb53c29926abde53f9a655558d57b84
Marc Tamlyn <marc.tamlyn@gmail.com>
2016-05-03 20:17:23 +0100

    Add active filter to membership admin.

--- membership/admin.py
+++ membership/admin.py
@@ -8,7 +8,7 @@ from .models import Member
 @admin.register(Member)
 class MemberAdmin(DjangoObjectActions, admin.ModelAdmin):
     list_display = ['name', 'age', 'membership_type', 'active', 'squad', 'agb_number']
-    list_filter = ['membership_type', 'age', 'squad']
+    list_filter = ['active', 'membership_type', 'age', 'squad']
     readonly_fields = ['created', 'modified']
     actions = objectactions = ['update_plan']
 
