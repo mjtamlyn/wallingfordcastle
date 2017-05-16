d5fc680b03b4ec9a7ac39d9dd48093f27e90d2f3
Marc Tamlyn <marc.tamlyn@gmail.com>
2017-05-16 21:46:40 +0100

    Payment flow.

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.http import Http404
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import (
-    CreateView, DeleteView, FormView, TemplateView, UpdateView,
+    CreateView, DeleteView, FormView, TemplateView, UpdateView, View,
 )
 
+import stripe
 from braces.views import LoginRequiredMixin, MessageMixin
 
 from .forms import EntryForm
@@ -18,6 +21,7 @@ class TournamentHome(TemplateView):
         if self.request.user.is_authenticated():
             context['existing_entries'] = self.request.user.entry_set.all()
             context['to_pay'] = self.request.user.entry_set.filter(paid=False).count() * 15
+            context['STRIPE_KEY'] = settings.STRIPE_KEY
             context['entry_form'] = EntryForm()
         return context
 
@@ -37,6 +41,7 @@ class EntryCreate(LoginRequiredMixin, MessageMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        self.messages.success('Entry added, please pay below.')
         return super().form_valid(form)
 
     def get_success_url(self):
@@ -71,5 +76,28 @@ class EntryDelete(LoginRequiredMixin, DeleteView):
         return reverse('tournaments:home')
 
 
-class Pay(FormView):
-    pass
+class Pay(LoginRequiredMixin, MessageMixin, View):
+    def post(self, request, *args, **kwargs):
+        token = request.POST['stripeToken']
+        if self.request.user.customer_id:
+            customer = stripe.Customer.retrieve(self.request.user.customer_id)
+            source = customer.sources.create(source=token)
+            customer.save()
+        else:
+            customer = stripe.Customer.create(
+                email=self.request.user.email,
+            )
+            source = customer.sources.create(source=token)
+            self.request.user.customer_id = customer.id
+            self.request.user.save()
+        to_pay = self.request.user.entry_set.filter(paid=False).count() * 1500
+        stripe.Charge.create(
+            amount=to_pay,
+            currency="GBP",
+            description="Wallingford Castle Archers Tournament Fee",
+            customer=customer.id,
+            source=source.id,
+        )
+        to_pay = self.request.user.entry_set.update(paid=True)
+        self.messages.success('Thanks! You will receive a confirmation email soon.')
+        return redirect('tournaments:home')
