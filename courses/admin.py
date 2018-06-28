95ab5a1725ec6ce8231e7cf757643e71e8d8cdf9
Marc Tamlyn <marc.tamlyn@gmail.com>
2018-06-28 14:23:17 +0100

    Add Summer 2018 course model.

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import CourseSignup
+from .models import CourseSignup, Summer2018Signup
+
+
+@admin.register(Summer2018Signup)
+class Summer2018SignupAdmin(admin.ModelAdmin):
+    list_display = ['student_name', 'email', 'student_date_of_birth', 'dates', 'group', 'paid']
+    list_filter = ['group']
 
 
 @admin.register(CourseSignup)
-class CourseSignup(admin.ModelAdmin):
+class CourseSignupAdmin(admin.ModelAdmin):
     list_display = ['student_name', 'email', 'student_date_of_birth', 'paid']
--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
@@ -12,6 +13,25 @@ class CourseSignup(models.Model):
     notes = models.TextField(blank=True, default='')
     gdpr_consent = models.BooleanField(default=False)
     contact = models.BooleanField(default=False)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.student_name
+
+
+class Summer2018Signup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    contact_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    contact_number = models.CharField(max_length=20)
+    student_name = models.CharField(max_length=255)
+    student_date_of_birth = models.DateField()
+    dates = ArrayField(models.DateField())
+    group = models.CharField(max_length=30)
+    experience = models.TextField(blank=True, default='')
+    notes = models.TextField(blank=True, default='')
+    gdpr_consent = models.BooleanField(default=False)
+    contact_consent = models.BooleanField(default=False)
     paid = models.BooleanField(default=False)
 
     def __str__(self):
