4d98ae2836452cb08a44407407eaa9cd99d3c871
Marc Tamlyn <marc.tamlyn@gmail.com>
2018-09-13 09:32:38 +0100

    Add home ed page.

--- courses/urls.py
+++ courses/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('summer-2018/<id>/payment/', views.Summer2018Payment.as_view(), name='summer-2018-payment'),
     path('dgs/', views.SchoolSignup.as_view(school='dgs'), name='dgs'),
     path('icknield/', views.SchoolSignup.as_view(school='icknield'), name='icknield'),
+    path('home-ed/', views.SchoolSignup.as_view(school='home-ed'), name='home-ed'),
     path('juniors/minis/', views.MinisInterestView.as_view(), name='minis-interest'),
     path('members/courses/', views.MembersCourseList.as_view(), name='members-course-list'),
     path('members/courses/<pk>/book/', views.MembersCourseBooking.as_view(), name='members-course-booking'),
