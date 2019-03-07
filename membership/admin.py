7fabf3ca954e254a5d98fa0dc638dd973b3c0a16
Marc Tamlyn <marc.tamlyn@gmail.com>
2019-03-07 10:34:00 +0000

    Add a form to directly book members into courses.
    Useful when they don't get round to it themselves, and simplifies the
    process for new members joining from minis/HE/etc

--- membership/admin.py
+++ membership/admin.py
@@ -1,9 +1,66 @@
+import functools
+
+from django import forms
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.views.generic import FormView
+from django.urls import path, reverse
 
 from django_object_actions import DjangoObjectActions, takes_instance_or_queryset
 
+from courses.models import Attendee, Course
 from wallingford_castle.admin import ArcherDataMixin
 from .models import Member
+
+
+class AdminBookCourseForm(forms.Form):
+    course = forms.ModelChoiceField(
+        Course.objects,
+        widget=forms.RadioSelect,
+        required=True,
+    )
+
+    def __init__(self, members, **kwargs):
+        self.members = members
+        super().__init__(**kwargs)
+
+    def save(self):
+        course = self.cleaned_data['course']
+        for member in self.members:
+            member.archer.user.add_invoice_item(
+                amount=course.members_price * 100,
+                description='%s for %s' % (course, member),
+            )
+            Attendee.objects.create(
+                course=course,
+                archer=member.archer,
+                paid=True,
+            )
+
+
+class AdminBookCourseView(FormView):
+    template_name = 'admin/membership/member/book_course.html'
+    form_class = AdminBookCourseForm
+
+    def get_form_kwargs(self):
+        selected = self.request.GET['ids'].split(',')
+        self.members = Member.objects.filter(id__in=selected)
+        kwargs = super().get_form_kwargs()
+        kwargs['members'] = self.members
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['opts'] = Member._meta
+        context['members'] = self.members
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('admin:membership_member_changelist')
 
 
 @admin.register(Member)
@@ -19,9 +76,26 @@ class MemberAdmin(DjangoObjectActions, ArcherDataMixin, admin.ModelAdmin):
         ('membership_type', 'level'),
         ('interest', 'created', 'modified'),
     ]
-    actions = objectactions = ['update_plan']
+    actions = objectactions = ['update_plan', 'book_into_course']
     search_fields = ['archer__name']
     autocomplete_fields = ['archer']
+
+    def get_urls(self):
+        urls = super().get_urls()
+
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            wrapper.model_admin = self
+            return functools.update_wrapper(wrapper, view)
+
+        info = self.model._meta.app_label, self.model._meta.model_name
+
+        urls.insert(
+            0,
+            path('book_course/', wrap(AdminBookCourseView.as_view()), name='%s_%s_book_course' % info),
+        )
+        return urls
 
     @takes_instance_or_queryset
     def update_plan(self, request, queryset):
@@ -29,3 +103,10 @@ class MemberAdmin(DjangoObjectActions, ArcherDataMixin, admin.ModelAdmin):
             member.archer.user.update_subscriptions()
     update_plan.short_description = 'Update plan'
     update_plan.label = 'Update plan'
+
+    @takes_instance_or_queryset
+    def book_into_course(self, request, queryset):
+        url = reverse('admin:%s_%s_book_course' % (self.model._meta.app_label, self.model._meta.model_name))
+        return HttpResponseRedirect(url + '?ids=%s' % ','.join(str(item.pk) for item in queryset))
+    book_into_course.short_description = 'Book into course'
+    book_into_course.label = 'Book into course'
