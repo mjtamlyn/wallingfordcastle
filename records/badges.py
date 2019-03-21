18e7b2d8bd67d63ecf910497bd644143749145b1
Marc Tamlyn <marc.tamlyn@gmail.com>
2019-03-21 09:45:26 +0000

    Add outdoor badges.

--- records/badges.py
+++ records/badges.py
@@ -27,6 +27,7 @@ class Badge:
 WA_BEGINNERS = BadgeGroup(name='WA beginners')
 CLUB_PORTSMOUTH = BadgeGroup(name='Half Portsmouth')
 CLUB_WA_18 = BadgeGroup(name='Half WA 18')
+CLUB_OUTDOOR_250 = BadgeGroup(name='Outdoor 250 Award')
 BADGES = [
     Badge(name='Red Feather', group=WA_BEGINNERS),
     Badge(name='Gold Feather', group=WA_BEGINNERS),
@@ -45,6 +46,12 @@ BADGES = [
     Badge(name='Red Half WA 18 (225)', group=CLUB_WA_18),
     Badge(name='Gold Half WA 18 (250)', group=CLUB_WA_18),
     Badge(name='Purple Half WA 18 (275)', group=CLUB_WA_18),
+    Badge(name='White Outdoor 250 Award (20m)', group=CLUB_OUTDOOR_250),
+    Badge(name='Black Outdoor 250 Award (30m)', group=CLUB_OUTDOOR_250),
+    Badge(name='Blue Outdoor 250 Award (40m)', group=CLUB_OUTDOOR_250),
+    Badge(name='Red Outdoor 250 Award (50m)', group=CLUB_OUTDOOR_250),
+    Badge(name='Gold Outdoor 250 Award (60m)', group=CLUB_OUTDOOR_250),
+    Badge(name='Purple Outdoor 250 Award (70m)', group=CLUB_OUTDOOR_250),
 ]
 
 BADGE_GROUPS = [
