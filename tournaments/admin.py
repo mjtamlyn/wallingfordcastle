f032e96c0f6d0f3350640daed3e9dee1296b0b1f
Marc Tamlyn <marc.tamlyn@gmail.com>
2021-05-18 12:05:20 +0100

    Add email address to the tournament entry table.

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -12,5 +12,5 @@ class TournamentAdmin(admin.ModelAdmin):
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'paid', 'club', 'gender', 'bowstyle', 'agb_number', 'round', 'notes']
+    list_display = ['name', 'user', 'paid', 'club', 'gender', 'bowstyle', 'agb_number', 'round', 'notes']
     list_filter = ['tournament', 'paid', 'gender', 'bowstyle', 'round']
