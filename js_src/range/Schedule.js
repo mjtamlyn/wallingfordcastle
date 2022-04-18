2a0f0f534def63d5f1e645183a211243df7b5a44
Marc Tamlyn <marc.tamlyn@gmail.com>
2022-04-18 09:25:24 +0100

    A start on absence reporting.

--- js_src/range/Schedule.js
+++ js_src/range/Schedule.js
@@ -1,43 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import moment from 'moment';
 
-class BaseBookedSlot extends React.Component {
-    render() {
-        const { slot, rowSpan, colSpan } = this.props;
+const BaseBookedSlot = ({ slot, rowSpan, colSpan, match }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-        let cancelLink = null;
-        if (slot.editable) {
-            const date = this.props.match.params.date;
-            const linkTarget = `/${date}/cancel/${slot.reference()}/`;
-            cancelLink = (
-                <Link
-                    className="range-schedule__cancel"
-                    to={ linkTarget }
-                >
-                    Cancel
-                </Link>
-            );
-        }
-
-        let title = null;
-        if (slot.groupName) {
-            title = <p className="range-schedule__title">{ slot.groupName }</p>
-        }
-        return (
-            <td
-                className="range-schedule__slot range-schedule__slot--booked"
-                rowSpan={ rowSpan }
-                colSpan={ colSpan }
+    let cancelLink = null;
+    if (slot.editable) {
+        const date = match.params.date;
+        const linkTarget = `/${date}/cancel/${slot.reference()}/`;
+        cancelLink = (
+            <Link
+                className="range-schedule__cancel"
+                to={ linkTarget }
             >
-                { title }
-                <p className="range-schedule__description">{ slot.details.names }</p>
-                <p className="range-schedule__description">{ slot.details.distance }</p>
-                { cancelLink }
-            </td>
+                Cancel
+            </Link>
         );
     }
-}
+
+    let title = null;
+    if (slot.groupName) {
+        title = <p className="range-schedule__title">{ slot.groupName }</p>
+    }
+
+    let toolsClass = 'range-schedule__tools';
+    if (menuOpen) {
+        toolsClass += ' range-schedule__tools--active';
+    }
+
+    return (
+        <td
+            className="range-schedule__slot range-schedule__slot--booked"
+            rowSpan={ rowSpan }
+            colSpan={ colSpan }
+        >
+            <a className={ toolsClass } onClick={ () => setMenuOpen(!menuOpen) }>Info</a>
+            { menuOpen &&
+                <div className="range-schedule__menu">
+                    <ul className="range-schedule__menu__list">
+                        <li className="range-schedule__menu__item">Report absence</li>
+                        <li className="range-schedule__menu__item">Book in</li>
+                        <li className="range-schedule__menu__item">View schedule</li>
+                    </ul>
+                </div>
+            }
+            { title }
+            <p className="range-schedule__description">{ slot.details.names }</p>
+            <p className="range-schedule__description">{ slot.details.distance }</p>
+            { cancelLink }
+        </td>
+    );
+};
 
 const BookedSlot = withRouter(BaseBookedSlot);
 
