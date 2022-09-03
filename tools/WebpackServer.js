514ade496960aef91763a5c8e4bccccb2a99c2f1
Marc Tamlyn <marc.tamlyn@gmail.com>
2022-09-03 14:48:58 +0100

    Minor new webpack tweaks.

--- tools/WebpackServer.js
+++ tools/WebpackServer.js
@@ -3,7 +3,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('../webpack.config');
 
 const compiler = Webpack(config);
-const devServerOptions = { ...config.devServer, open: true, port: 3000 };
+const devServerOptions = { ...config.devServer, port: 3000 };
 const server = new WebpackDevServer(devServerOptions, compiler);
 
 const runServer = async () => {
