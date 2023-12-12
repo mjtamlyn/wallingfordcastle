c95357930b7269a3c5c867a15c9279ea123102e8
Marc Tamlyn <marc.tamlyn@gmail.com>
2023-12-12 11:02:43 +0000

    Remove really pointless uses of React.Component.
    There's still quite a few but they need rewriting with hooks

--- js_src/utils/NoMatch.js
+++ js_src/utils/NoMatch.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-class NoMatch extends React.Component {
-    render() {
-        return (
-            <h2>Oops - Page not found</h2>
-        );
-    }
+const NoMatch = () => {
+    return (
+        <h2>Oops - Page not found</h2>
+    );
 }
 
 export default NoMatch;
